98a3ee5cf7c96a8c3e143a55207bf0745cbade15
Nico de Leon <nico@ns.gg>
2015-03-24 22:25:28 +0800

    update setup SQL to match MySQL

--- database.sql
+++ database.sql
@@ -1,24 +1,41 @@
-create table users(
-  username varchar(20) primary key,
-  password varchar(20),
-  isAdmin BOOLEAN
+-- MySQL
+
+create table users (
+  id int auto_increment primary key,
+  username varchar(20) not null unique,
+  password char(64) not null, -- hash
+  is_admin boolean not null
   );
-  
-create table card_set(
-  set_id integer auto_increment primary key,
-  title varchar(50),
-  description varchar(200),
-  language_1 varchar(20),
-  language_2 varchar(20)
-  foreign key user_id varchar(20) references user(username));
-  
-create table card(
-  card_id integer auto_increment primary key,
-  word varchar(30),
-  translation varchar(60),
-  foreign key set_id integer references card_set(set_id) on delete cascade);
-  
-create table favorite(
-  foreign key user_id varchar(20) references user(username),
-  foreign key set_id integer references card_set(set_id) on delete cascade,
-  primary key(user_id,set_id));
+
+create table languages (
+  id int auto_increment primary key,
+  name varchar(50) not null unique
+  );
+
+create table card_sets (
+  id int auto_increment primary key,
+  title varchar(50) not null,
+  description varchar(200) not null,
+  language1_id int not null,
+  language2_id int not null,
+  user_id int not null,
+  foreign key (language1_id) references languages(id) on delete cascade,
+  foreign key (language2_id) references languages(id) on delete cascade,
+  foreign key (user_id) references users(id) on delete cascade
+  );
+
+create table cards (
+  id int auto_increment primary key,
+  word1 varchar(50) not null,
+  word2 varchar(50) not null,
+  set_id int not null,
+  foreign key (set_id) references card_sets(id) on delete cascade
+  );
+
+create table favorites (
+  user_id int not null,
+  set_id int not null,
+  foreign key (user_id) references users(id) on delete cascade,
+  foreign key (set_id) references card_sets(id) on delete cascade,
+  primary key (user_id, set_id)
+  );
