dfb9a33a6f2db6f4968fd68a66cae265d50203cc
Nico de Leon <nico@ns.gg>
2015-03-25 12:46:20 +0800

    Handle logins

--- common.inc.php
+++ common.inc.php
@@ -8,26 +8,38 @@ if ($mysqli->connect_errno) {
     exit(1);
 }
 
-if (isset($_COOKIE['user'])) {
+function check_login($user, $pass) {
     if ($stmt = $mysqli->prepare(
-        "SELECT id, username, is_admin FROM users WHERE username = ?")) {
+        "SELECT id, username, is_admin FROM users WHERE username = ? AND password = ?")) {
 
-        $stmt->bind_param("s", $_COOKIE['user']);
+        $stmt->bind_param("ss", $user, $pass);
         $stmt->execute();
         $stmt->bind_result($user_id, $user_name, $user_is_admin);
 
         if ($stmt->fetch()) {
             define('USER_ID', $user_id);
-            define('USER_NAME', $user_name);
+            define('USER_NAME', $user_name); // gets correct capitalization from DB
             define('USER_IS_ADMIN', $user_is_admin);
             define('USER_IS_LOGGED_IN', true);
+            return true;
         } else {
             define('USER_IS_LOGGED_IN', false);
+            return false;
         }
     } else {
         echo "Unable to fetch users.";
         exit(1);
     }
+
+}
+
+if (isset($_POST['login'])) {
+    if (check_login($_POST['user'], $_POST['pass'])) {
+        setcookie('user', $_POST['user']);
+        setcookie('pass', $_POST['pass']);
+    }
+} elseif (isset($_COOKIE['user'])) {
+    check_login($_COOKIE['user'], $_COOKIE['pass']);
 } else {
     define('USER_IS_LOGGED_IN', false);
 }
