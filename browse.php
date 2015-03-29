4aa39a699771d4747b454891a1239698fd1f9643
Nico de Leon <nico@ns.gg>
2015-03-29 18:52:46 +0800

    One page of search results

--- browse.php
+++ browse.php
@@ -101,27 +101,6 @@ $search_results = search_sets($mysqli, $_GET['title'], $_GET['description'], $_G
               <p>Set description</p>
             </li>
           </ul>
-
-          <nav>
-            <ul class="pagination">
-              <li>
-                <a href="#" aria-label="Previous">
-                  <span aria-hidden="true">&laquo;</span>
-                </a>
-              </li>
-              <li><a href="#">1</a></li>
-              <li><a href="#">2</a></li>
-              <li><a href="#">3</a></li>
-              <li><a href="#">4</a></li>
-              <li><a href="#">5</a></li>
-              <li>
-                <a href="#" aria-label="Next">
-                  <span aria-hidden="true">&raquo;</span>
-                </a>
-              </li>
-            </ul>
-          </nav>
-
         </div>
       </div>
 
