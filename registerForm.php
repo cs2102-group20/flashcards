4ca759d13e11f0468ed32e4269d00013db5df465
SandyLam22 <sandlamlry@gmail.com>
2015-04-06 05:05:08 +0800

    debugging

--- registerForm.php
+++ registerForm.php
@@ -6,9 +6,13 @@ require_once 'common.inc.php';
 //insert data if ok
 if(isset($_POST['username'])){
 	$sql="SELECT * FROM users WHERE username='" . $_POST['username'] . "';";
+	if ($mysqli->query($sql) === TRUE) {
 	$mysql_get_users = $mysqli->query($sql);
+	}else {
+		echo "Error: " . $sql . "<br>" . $mysqli->error;
+	}
 	$get_rows = mysql_affected_rows($mysqli);
-
+	echo $get_rows;
 	if($get_rows >=1){
 	echo "user exists";
 	//die();
