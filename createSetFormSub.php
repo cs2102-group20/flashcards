eed47bc93dbd38813460ca80e77aab04138984f2
SandyLam22 <sandlamlry@gmail.com>
2015-04-06 19:53:25 +0800

    get user id

--- createSetFormSub.php
+++ createSetFormSub.php
@@ -12,6 +12,14 @@
 			echo "<a href='register.php'>Please try registering again.</a>";
 		}
 	}
+	function get_userId($mysqli, $username){
+		if ($result = $mysqli->query('SELECT id FROM users WHERE username=' . $username) {
+        return $result->fetch_all(MYSQLI_ASSOC);
+		} else {
+			echo "Unable to fetch user id.";
+			exit(1);
+		}
+	}
 	
 	$languages = get_languages($mysqli);
 		echo $_GET['langWord'];
@@ -23,7 +31,12 @@
 	  if(!isset($_GET['langTranslation']) || $value['id'] == $_GET['langTranslation'])$language_id_translation=$value['id'];
 	}
 	//$language_id_translation = implode('', array_map(function ($language) { return ($language['selected']) ? $language['id'] : ''; }, $languages));
-	insert_sets($mysqli, $_GET['title'], $_GET['description'], $language_id_word, $language_id_translation, $_COOKIE['user']);
+	//GET user id
+	$userId_array=get_userId($mysqli, $_COOKIE['user']);
+	foreach ($languages as $value) {
+		$userId=$value['id'];
+	}
+	insert_sets($mysqli, $_GET['title'], $_GET['description'], $language_id_word, $language_id_translation, $userId);
 
 
 ?>
