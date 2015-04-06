b50b7d421484e7615cc2bc03f311d83dcb9b37ab
SandyLam22 <sandlamlry@gmail.com>
2015-04-06 18:51:23 +0800

    test

--- createSet.php
+++ createSet.php
@@ -21,9 +21,7 @@
     <![endif]-->
 	<?php
 	$languages = get_languages($mysqli);
-	foreach ($languages as $key => $value) {
-	  $languages[$key]['selected'] = !isset($_GET['languages']) || in_array($value['id'], $_GET['languages']);
-	}
+	
 	?>
   </head>
 
@@ -59,9 +57,17 @@
 				<tr height='25' ><td width='50' align='center'>&nbsp;</td><td width='300' align='center'><font size='3'>Word</font></td><td width='500' align='center'><font size='3'>Translation</font></td></tr>
 				<tr height='25' ><td width='50' align='center'>&nbsp;</td><td width='300' align='center'><font size='2'>Word Language</font></td><td width='500' align='center'><font size='2'>Translation Language</font></td></tr>
 				<tr height='40' ><td width='50' align='center'>&nbsp;</td><td width='300' align='center'>
-					<select id='langWord' name='langWord' required><?php foreach ($languages as $language) { ?><option value="<?php echo $language['id']; ?>" <?php if ($language['selected']) echo 'selected'; ?>><?php echo htmlspecialchars($language['name']); ?></option><?php } ?></select>
+					<select id='langWord' name='langWord' required>
+					<?php foreach ($languages as $language) { ?>
+					<option value="<?php echo $language['id']; ?>" ><?php echo htmlspecialchars($language['name']); ?></option>
+					<?php } ?>
+					</select>
 					</td><td width='500' align='center'>
-					<select id='langTranslation' name='langTranslation' required><?php foreach ($languages as $language) { ?><option value="<?php echo $language['id']; ?>" <?php if ($language['selected']) echo 'selected'; ?>><?php echo htmlspecialchars($language['name']); ?></option><?php } ?></select>
+					<select id='langTranslation' name='langTranslation' required>
+					<?php foreach ($languages as $language) { ?>
+					<option value="<?php echo $language['id']; ?>" ><?php echo htmlspecialchars($language['name']); ?></option>
+					<?php } ?>
+					</select>
 				</td></tr>
 			</table>
 			<input type="image" src="images/addBtn.png" style="width: 30px; height: 30px" onclick="addEntry();return false;"/> 
