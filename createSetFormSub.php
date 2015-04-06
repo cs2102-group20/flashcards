c254dd9b48d5d0978c6254aadea764105032f0c7
SandyLam22 <sandlamlry@gmail.com>
2015-04-06 19:22:56 +0800

    test

--- createSetFormSub.php
+++ createSetFormSub.php
@@ -20,9 +20,9 @@
 	}
 	//$language_id_word = implode('', array_map(function ($language) { return ($language['selected']) ? $language['id'] : ''; }, $languages));
 	foreach ($languages as $key => $value) {
-	  $languages[$key]['selected'] = !isset($_GET['langTranslation']) || in_array($value['id'], $_GET['langTranslation']);
+	  if(!isset($_GET['langTranslation']) || $value['id'] == $_GET['langTranslation'])$language_id_translation=$value['id'];
 	}
-	$language_id_translation = implode('', array_map(function ($language) { return ($language['selected']) ? $language['id'] : ''; }, $languages));
+	//$language_id_translation = implode('', array_map(function ($language) { return ($language['selected']) ? $language['id'] : ''; }, $languages));
 	insert_sets($mysqli, $_GET['title'], $_GET['description'], $language_id_word, $language_id_translation, $_COOKIE['user']);
 
 
